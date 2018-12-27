29ca9d3a3b0fd357dde1ba48664a0361640635f5
tim <tim.kambic12@gmail.com>
2018-12-27 11:29:47 +0100

    code for evaluating FR and PAD from all methods

---
+++ evaluate_all.py
@@ -0,0 +1,68 @@
+import numpy as np
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+
+
+def evaluate(score_file, true_labels_file):
+    THRESHOLD = 0
+    score_list = np.loadtxt(score_file)
+    true_labels = np.loadtxt(true_labels_file)
+    NC = np.where(true_labels >= 0)[0].size  # n of true clients
+    NI = np.where(true_labels < 0)[0].size  # number of impostors
+    print(NC, NI)
+
+    FA = FR = 0
+    for i in range(score_list.size):
+        true_label = true_labels[i]
+        label = 1 if score_list[i] > THRESHOLD else -1
+        if label == -1 and true_label == 1:
+            FR += 1
+        elif label == 1 and true_label == -1:
+            FA += 1
+
+    # print(FA, FR)
+    FAR = FA / NI
+    FRR = FR / NC
+    HTER = (FAR + FRR) / 2
+
+    # print("FAR:", FAR)
+    # print("FRR:", FRR)
+    # print("HTER:", HTER)
+
+    fpr, tpr, threshold = roc_curve(true_labels, score_list)
+    roc_auc = auc(fpr, tpr)
+
+    fnr = 1 - tpr
+    eer_threshold = threshold[np.nanargmin(np.absolute((fnr - fpr)))]
+    EER = fpr[np.nanargmin(np.absolute((fnr - fpr)))]
+    # print("EER:", EER)
+
+    return fpr, tpr, roc_auc, HTER, EER
+
+
+fpr_replay,tpr_replay,auc_replay,HTER_replay,EER_replay = evaluate("scores/scores_model1_Replay_20122018.txt", "scores/truelabels_model1_Replay_20122018.txt")
+fpr_replay2,tpr_replay2,auc_replay2,HTER_replay2,EER_replay2 = evaluate("scores/scores_model2_Replay_20122018.txt", "scores/truelabels_model2_Replay_20122018.txt")
+fpr_replay_oulu2,tpr_replay_oulu2,auc_replay_oulu2,HTER_replay_oulu2,EER_replay_oulu2 = evaluate("scores/scores_model2_Replay_dataOuluP1_20122018.txt", "scores/truelabels_model2_Replay_dataOuluP1_20122018.txt")
+# fpr_replay3,tpr_replay3,auc_replay3,HTER_replay3,EER_replay3 = evaluate("scores/scores_model3_Replay_28122018.txt", "scores/truelabels_model3_Replay28122018.txt")
+# fpr_replay_oulu3,tpr_replay_oulu3,auc_replay_oulu3,HTER_replay_oulu3,EER_replay_oulu3 = evaluate("scores/scores_model3_Replay_Oulu_p1_28122018.txt", "scores/true_labels_model3_Replay_28122018.txt")
+
+
+print("replay model1 - HTER: %0.3f , EER: %0.3f" % (HTER_replay,EER_replay))
+print("replay model2 - HTER: %0.3f , EER: %0.3f" % (HTER_replay2,EER_replay2))
+print("replay+oulu model2 - HTER: %0.3f , EER: %0.3f" % (HTER_replay_oulu2,EER_replay_oulu2))
+# print("replay model3 - HTER: %0.3f , EER: %0.3f" % (HTER_replay3,EER_replay3))
+# print("replay+oulu model3 - HTER: %0.3f , EER: %0.3f" % (HTER_replay_oulu3,EER_replay_oulu3))
+
+
+plt.plot([0, 1], [0, 1], color='navy', lw=1, linestyle='--')
+plt.plot(fpr_replay, tpr_replay, color='darkorange', lw=2, label='ReplayAttack model1 (area = %0.3f)' % auc_replay)
+plt.plot(fpr_replay2, tpr_replay2, color='blue', lw=2, label='ReplayAttack model2 (area = %0.3f)' % auc_replay2)
+plt.plot(fpr_replay_oulu2, tpr_replay_oulu2, color='green', lw=2, label='ReplayAttack+Oulu model2 (area = %0.3f)' % auc_replay_oulu2)
+# plt.plot(fpr_replay3, tpr_replay3, color='green', lw=2, label='ReplayAttack model3 (area = %0.3f)' % auc_replay3)
+# plt.plot(fpr_replay_oulu3, tpr_replay_oulu3, color='green', lw=2, label='ReplayAttack+Oulu model3 (area = %0.3f)' % auc_replay_oulu3)
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.grid(True)
+plt.show()
---
+++ evaluate_fr_all.py
@@ -0,0 +1,97 @@
+import numpy as np
+from sklearn.metrics import roc_curve, auc
+import matplotlib.pyplot as plt
+
+def euclideanDistance(source_representation, test_representation):
+    euclidean_distance = source_representation - test_representation
+    euclidean_distance = np.sum(np.multiply(euclidean_distance, euclidean_distance))
+    euclidean_distance = np.sqrt(euclidean_distance)
+    return euclidean_distance
+
+test_pairs = [
+    [2,3],
+    [4,5],
+    [10,11],
+    [19,23],
+    [29,34],
+    [35,44],
+    [28,45],
+    [46,51],
+    [25,61],
+    [55,67],
+    [14,70],
+    [24,72],
+    [33,74],
+    [105,2601],
+    [2985,1360],
+    [1368,2203],
+    [2407,1080],
+    [1098,258],
+    [3197,3160],
+    [3170,1903],
+    [1555,504],
+    [113,1456],
+    [1472,2989],
+    [3190,1850],
+    [1599,846],
+    [492,493],
+    [509,105],
+    [2549,2083],
+    [2097,2054],
+    [1308,1295],
+    [974,982],
+    [970,969],
+    [1005,540],
+    [530,651],
+    [544,571],
+    [301,332],
+    [2700,2114],
+    [1684,1685],
+    [1690,1691],
+    [1704,1705],
+    [1688,1674],
+    [980,1002]
+]
+def ROCcurve(scores,ids):
+    distance_list = []
+    label_list = []
+
+    for x in test_pairs:
+        idx1 = x[0]
+        idx2 = x[1]
+        f1 = scores[idx1]
+        f2 = scores[idx2]
+        distance = euclideanDistance(f1, f2)
+        label = 0
+        if ids[idx1] == ids[idx2]:
+            label = 1
+        distance_list.append(1 / distance)
+        label_list.append(label)
+
+    fpr, tpr, threshold = roc_curve(label_list, distance_list)
+    return fpr,tpr,threshold
+
+scores1 = np.loadtxt("scores/features_orgVggFace.txt")
+ids_one_hot = np.loadtxt("scores/IDs_onehot")
+ids1 = [np.argmax(x) for x in ids_one_hot]
+scores2 = np.loadtxt("scores/features_orgVggFace.txt") #TODO
+ids_one_hot = np.loadtxt("scores/IDs_onehot")
+ids2 = [np.argmax(x) for x in ids_one_hot]
+# np.savetxt("scores/IDs",ids, fmt='%d')
+
+
+fpr1, tpr1,_ = ROCcurve(scores1,ids1)
+fpr2, tpr2,_ = ROCcurve(scores2,ids2)
+roc_auc1 = auc(fpr1, tpr1)
+roc_auc2 = auc(fpr2, tpr2)
+
+
+plt.plot([0, 1], [0, 1], color='navy', lw=1, linestyle='--')
+plt.plot(fpr1, tpr1, color='darkorange', lw=2, label='original (area = %0.2f)' % roc_auc1)
+plt.plot(fpr2, tpr2, color='green', lw=2, label='pad finetunning (area = %0.2f)' % roc_auc1)
+plt.xlabel('False Positive Rate')
+plt.ylabel('True Positive Rate')
+plt.title('Receiver operating characteristic')
+plt.legend(loc="lower right")
+plt.grid(True)
+plt.show()
